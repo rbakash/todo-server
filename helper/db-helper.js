5902ab1f94c4b032b4153b2a1400d3572bd88cbc
Akash RB <rbakashcs@gmail.com>
2021-04-30 13:44:53 -0500

    Debuggin the conncetion refused issue for google sql instance

--- helper/db-helper.js
+++ helper/db-helper.js
@@ -5,7 +5,14 @@ dotenv.config({
     path: '../config.env'
 });
 
-
+// Debuggint the connection refused for the google sql instance 
+console.log({
+  host:process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT
+});
 var devDB = mysql.createConnection({
   host:process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
