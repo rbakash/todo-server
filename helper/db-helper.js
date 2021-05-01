1691c94bffa718cdbab0e32940ba1d0613dd7695
Akash RB <rbakashcs@gmail.com>
2021-04-30 19:39:35 -0500

    Removed the console.log which was added to debug the deployment error

--- helper/db-helper.js
+++ helper/db-helper.js
@@ -6,13 +6,6 @@ dotenv.config({
 });
 
 // Debuggint the connection refused for the google sql instance 
-console.log({
-  host:process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT
-});
 var devDB = mysql.createConnection({
   host:process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
